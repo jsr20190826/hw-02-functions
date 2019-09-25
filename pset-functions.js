4ae75256790079c9156b743a9b3c1621302015ad
Lara Dominick <laraann@live.com>
2019-09-25 14:47:59 -0400

    Homework 2 submission

--- pset-functions.js
+++ pset-functions.js
@@ -31,6 +31,23 @@ console.log('Problem 1:')
 
 // Add your code below this line
 
+function blackJack(playerCardScore, dealerCardScore)  {
+
+  if (playerCardScore > 21 && dealerCardScore > 21) {
+    return 0
+  } else if (playerCardScore < 21 && dealerCardScore > 21)  {
+    return playerCardScore
+  } else if (playerCardScore > 21 && dealerCardScore < 21)  {
+    return dealerCardScore
+  } else if (playerCardScore > dealerCardScore) {
+    return playerCardScore
+  } else if (playerCardScore < dealerCardScore) {
+    return dealerCardScore
+  }
+}
+const playerCardScore = 22
+const dealerCardScore = 22
+console.log(blackJack(playerCardScore, dealerCardScore))
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -90,6 +107,23 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 2:')
 
 // Add your code below this line
+function wordCount(phrase) {
+  let words = phrase.split(" ")
+  let wordsMap = {}
+  words.forEach(function(key)  {
+    if (wordsMap.hasOwnProperty(key)) {
+      wordsMap[key]++
+    } else {
+      wordsMap[key] = 1
+    }
+  })
+  let keys = Object.keys(wordsMap)
+  for (key of keys) {
+    console.log(key + ":" + " " + wordsMap[key])
+  }
+}
+
+wordCount("Humpty Dumpty sat on a wall Humpty Dumpty had a great fall")
 
 // Add your code above this line
 
@@ -136,6 +170,26 @@ console.log('Problem 3:')
 
 // Add your code below this line
 
+function scrabbleScore(word) {
+    let letters = {
+        "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
+        "D": 2, "G": 2,
+        "B": 3, "C": 3, "M": 3, "P": 3,
+        "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+        "K": 5,
+        "J": 8, "X": 8,
+        "Q": 10, "Z": 10
+    }
+    wordArray = Array.from(word)
+    score = 0
+    for (letter of wordArray) {
+        upperCaseLetter = letter.toUpperCase()
+        score = score + letters[upperCaseLetter]
+    }
+    console.log(score)
+}
+scrabbleScore("cabbage")
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -174,6 +228,16 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 4:')
 
 // Add your code below this line
+function isPalindrome(word) {
+  let reverseString = word.split("").reverse().join("")
+  if (word === reverseString) {
+    console.log("true")
+  } else {
+    console.log("false")
+    }
+}
+
+isPalindrome("racecar")
 
 // Add your code above this line
 
@@ -207,6 +271,20 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 5:')
 
 // Add your code below this line
+function doubleLetters(word) {
+    result = false
+    letters = Array.from(word)
+    for (let i = 0; i < letters.length; i++) {
+        if (i + 1 < letters.length) {
+            letter = letters[i]
+            if (letter == letters[i + 1]) {
+                result = true
+            }
+        }
+    }
+    console.log(result)
+}
+doubleLetters('loop')
 
 // Add your code above this line
 
